b9d4538cb4f42d5de5569cb184911d9f6915cd35
Fco Javier <fjdap80@gmail.com>
2022-10-14 17:16:54 +0200

    Error2

---
+++ Unidad1_PrimeraAppJavaFX/src/ch/makery/address/model/Person.java
@@ -0,0 +1,35 @@
+package ch.makery.address.model;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Person {
+	private final SimpleStringProperty firstName;
+	private final SimpleStringProperty lastName;
+	
+
+	public Person(String fName, String lName) {
+		this.firstName = new SimpleStringProperty(fName);
+		this.lastName = new SimpleStringProperty(lName);
+		
+	}
+
+	public String getFirstName() {
+		return firstName.get();
+	}
+
+	public void setFirstName(String fName) {
+		firstName.set(fName);
+	}
+
+	public String getLastName() {
+		return lastName.get();
+	}
+
+	public void setLastName(String lName) {
+		lastName.set(lName);
+	}
+
+	
+
+}
