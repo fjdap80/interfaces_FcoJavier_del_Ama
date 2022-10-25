8e419a66442fce0301c8ab01975208c6ee92a9ef
Fco Javier <fjdap80@gmail.com>
2022-10-25 21:05:44 +0200

    25/10

--- FcoJavier_DelAma_Perez_PracticaUnidades_1-2/src/ch/makery/address/model/Personas.java
+++ FcoJavier_DelAma_Perez_PracticaUnidades_1-2/src/ch/makery/address/model/Personas.java
@@ -4,44 +4,49 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Personas {
-	private final SimpleStringProperty firstName;
-	private final SimpleStringProperty lastName;
-	private final SimpleStringProperty email;
-	private final SimpleStringProperty localidad;
-	
-	
+	private SimpleStringProperty firstName;
+	private SimpleStringProperty lastName;
+	private SimpleStringProperty email;
+	private SimpleStringProperty localidad;
 
-	public Personas(String fName, String lName,String email,String localidad) {
+	public Personas(String fName, String lName, String email, String localidad) {
 		this.firstName = new SimpleStringProperty(fName);
 		this.lastName = new SimpleStringProperty(lName);
 		this.email = new SimpleStringProperty(email);
 		this.localidad = new SimpleStringProperty(localidad);
-		
+
 	}
 
-
-
-	public SimpleStringProperty getFirstName() {
-		return firstName;
+	public String getFirstName() {
+		return firstName.get();
 	}
 
-
-
-	public SimpleStringProperty getLastName() {
-		return lastName;
+	public String getLastName() {
+		return lastName.get();
 	}
 
-
-
-	public SimpleStringProperty getEmail() {
-		return email;
+	public String getEmail() {
+		return email.get();
 	}
 
-
-
-	public SimpleStringProperty getLocalidad() {
-		return localidad;
+	public String getLocalidad() {
+		return localidad.get();
 	}
 
-	
+	public void setFirstName(String firstName) {
+		this.firstName.set(firstName);
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName.set(lastName);
+	}
+
+	public void setEmail(String email) {
+		this.email.set(email);
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad.set(localidad);
+	}
+
 }
--- FcoJavier_DelAma_Perez_PracticaUnidades_1-2/src/ch/makery/address/view/ListaControladores.java
+++ FcoJavier_DelAma_Perez_PracticaUnidades_1-2/src/ch/makery/address/view/ListaControladores.java
@@ -39,6 +39,12 @@ public class ListaControladores {
 
     @FXML
     void initialize() {
+    	
+    	ColNombre.setCellValueFactory(new PropertyValueFactory<Personas,String>("firstName"));
+    	colApellidos.setCellValueFactory(new PropertyValueFactory<Personas,String>("lastName"));
+    	colEmail.setCellValueFactory(new PropertyValueFactory<Personas,String>("email"));
+    	colLocalidad.setCellValueFactory(new PropertyValueFactory<Personas,String>("localidad"));
+    	
         assert ColNombre != null : "fx:id=\"ColNombre\" was not injected: check your FXML file 'MainLayoutsClientes.fxml'.";
         assert colApellidos != null : "fx:id=\"colApellidos\" was not injected: check your FXML file 'MainLayoutsClientes.fxml'.";
         assert colEmail != null : "fx:id=\"colEmail\" was not injected: check your FXML file 'MainLayoutsClientes.fxml'.";
@@ -51,6 +57,10 @@ public class ListaControladores {
        		 new Personas("Mariano", "Ortega","Mariano@gmail.com","Sevilla"),
        		 new Personas("Roberto", "Jimenez","Roberto@gsumail.com","Albacete"),
        		 new Personas("Pepito", "Garcia","Pepito@gsumail.com","Valencia"),
+       		 new Personas("Juan", "Gomez","Juan@gsumail.com","Alicante"),
+       		 new Personas("Rocio", "Fernandez","Rocio@gsumail.com","Madrid"),
+       		 new Personas("Ana", "Romero","Ana@gsumail.com","Toledo"),
+       		 new Personas("Pedro", "Ramirez","Pedro@gsumail.com","Cuenca"),
        		 new Personas("Rosario", "Suarez","Rosario@gsumail.com","Romangordo")
        		 );
        
